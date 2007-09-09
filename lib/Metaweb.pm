dc2e25c68790cb3986559608bf60abacca6baff5
Alex Skud Bayley <skud@infotrope.net>
2007-09-09 08:52:35 +0000

    Import of SKUD/Metaweb-0.03 from CPAN.
    gitpan-cpan-distribution: Metaweb
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SKUD/Metaweb-0.03.tar.gz
    gitpan-cpan-author:       SKUD
    gitpan-cpan-maturity:     released

--- lib/Metaweb.pm
+++ lib/Metaweb.pm
@@ -29,11 +29,11 @@ Metaweb - Perl interface to the Metaweb/Freebase API
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
